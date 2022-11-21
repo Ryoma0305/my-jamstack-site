733944b83a794da43121033fbf8b77a4910561af
Ryoma0305 <ryomaaa0602@gmail.com>
2022-11-21 22:09:39 +0900

    commit

---
+++ src/library/wpcms-forbiz-programs.ts
@@ -0,0 +1,29 @@
+//型定義
+
+//APIの呼び出し
+export const getForbizPrograms = async () => {
+  //   return await client.get<BlogResponse>({ endpoint: "blogs", queries });
+  const data = await fetchAPI("", {});
+  console.log(data);
+  return { contents: data };
+};
+
+async function fetchAPI(query, { variables } = {}) {
+  const headers = { "Content-Type": "application/json" };
+  const res = await fetch(
+    "http://practicearimuraryomacom.local/wp-json/wp/api/forbiz_programs",
+    {
+      method: "GET",
+      headers,
+      //   body: JSON.stringify({ query, variables }),
+    }
+  );
+
+  const json = await res.json();
+  return json.map((item: any) => {
+    return {
+      program_title: item.program_title,
+      program_text: item.program_text,
+    };
+  });
+}
